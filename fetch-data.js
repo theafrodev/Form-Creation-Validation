5ed9decf8c43d2bb578081bf5c89800273e2ad5d
theafrodev <fialorchristian@gmail.com>
2024-07-19 14:55:39 +0000

    users fetch and display

---
+++ fetch-data.js
@@ -0,0 +1,41 @@
+const fetchUserData = async () => {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer = document.getElementById("api-data");
+
+    try{
+        const response = await fetch(apiUrl);
+
+        const users = await response.json();
+
+        dataContainer.innerHTML = ' ';
+
+        // console.log(users);
+
+        const userList = document.createElement('ul');
+
+        users.forEach(user => {
+
+            const listItem = document.createElement('li');
+            listItem.textContent = user.name;
+
+            userList.appendChild(listItem);
+
+            
+        });
+
+        dataContainer.appendChild(userList);
+
+
+    }catch{
+        dataContainer.innerHTML= ' ';
+        dataContainer.textContent = 'Failed to load user data.'
+    }
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchUserData();
+});
+
